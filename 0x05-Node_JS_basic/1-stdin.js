4210276cc2de604a1d10cef7efb99323dbddeb6a
SIHAM <sihamfichouche@gmail.com>
2025-01-10 20:25:09 +0000

    1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  // Convert input from buffer to string and remove newline
-  const name = input.toString().trim();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
 
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
